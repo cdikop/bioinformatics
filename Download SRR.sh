170d99b2fdbe7234acc40b8f15722489abc29f85
cdikop <yuanhengdai@163.com>
2022-11-04 18:21:06 +0800

    path

--- Download SRR.sh
+++ Download SRR.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ##########################################
 #   Program:
 #       Download SRR 
@@ -6,6 +7,7 @@
 #   Author:
 #       DTurtle
 ##########################################
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 for id in SRR_list.txt
 do 
 prefetch $id
