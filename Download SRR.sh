edb06ae2989b657ef858827b38e1f36fc46980f0
cdikop <yuanhengdai@163.com>
2022-11-05 15:26:23 +0800

    changes

--- Download SRR.sh
+++ Download SRR.sh
@@ -3,7 +3,7 @@
 #   Program:
 #       Download SRR 
 #   History:
-#       2022/11/4 1.0
+#       2022/11/5 1.1
 #   Author:
 #       DTurtle
 ##########################################
@@ -12,15 +12,15 @@ PATH=${PATH}:/sratoolkit.3.0.0-ubuntu64/bin # add the prefetch to PATH
 export $PATH
 ###################################################
 # list
-echo "Please input SRR list.txt"
+echo "Please input SRR list.txt："
 read SRR_list  # input the list
 ###########################################
-# circle
+# download
 cat $SRR_list | while read id
 do 
 prefetch $id
 echo $id
-echo $(time())   #输出一下运行时间
+echo $(time())   # running time
 done
 
 
