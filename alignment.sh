b0d151e6f3ca1ba4f132dca0c8b812bd720b6629
cdikop <yuanhengdai@163.com>
2022-11-08 16:22:56 +0800

    new

---
+++ alignment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##########################################
+#   Program:
+#       Transform SRR to fastq
+#   History:
+#       2022/11/8 1.1
+#   Author:
+#       DTurtle
+#################################################
+# PATH
+PATH=${PATH}:/home/data/sratoolkit.3.0.0-ubuntu64/bin # add the prefetch to PATH
+###################################################
