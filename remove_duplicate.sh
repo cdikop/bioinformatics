c423deb4141a0dd131930b5ba2676d377752c010
cdikop <yuanhengdai@163.com>
2022-11-10 08:46:42 +0800

    yes

---
+++ remove_duplicate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##########################################
+#   Program:
+#       remove duplicate
+#   History:
+#       2022/11/8 1.1
+#   Author:
+#       DTurtle
+#################################################
+# PATH
+PATH=${PATH}:/home/data/t190305/sratoolkit.3.0.0-ubuntu64/bin # add the prefetch to PATH
+export $PATH
+###################################################
